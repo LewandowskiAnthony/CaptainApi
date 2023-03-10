0945aeb6ab387d1a5395f728797a2de9e4a68e45
ANTHONY LEWANDOWSKI <lewandowski.anthony.pro@gmail.com>
2023-03-10 10:43:33 +0100

    feat(CaptainAPI) - Add multiple real endpoint tests

--- main.py
+++ main.py
@@ -6,30 +6,37 @@ from colorama import Style
 from datetime import datetime, timezone
 import requests as requests
 
-environment_json = open(sys.argv[1])
+json_path = open(sys.argv[1])
+
 
 class Api:
-    def __init__(self, url, key, env, test_data, name, field_to_check):
+    def __init__(self, url, key, env, name, tests):
         self.url = url
         self.key = key
         self.env = env
-        self.test_data = test_data
         self.name = name
-        self.field_to_check = field_to_check
+        self.tests = tests
 
 
-# Object containing API urls
-class ApiUrls:
-    def __init__(self, url, test_endpoint):
-        self.url = url
-        self.test_endpoint = test_endpoint
+# Object containing API tests
+class ApiTests:
+    def __init__(self, name, endpoint, value, must_be_present_field, http_method):
+        self.name = name
+        self.endpoint = endpoint
+        self.value = value
+        self.must_be_present_field = must_be_present_field
+        self.http_method = http_method
+        self.test_endpoint = "{}/{}".format(endpoint, value)
 
 
-data = json.load(environment_json)
+# Global variables definition
+# Json fields
+data = json.load(json_path)
 auth_information = data['authentication']
 running_information = data['running_variable']
 api_information = data['apis']
 
+# Debug mode to activate logs
 debug_mode = running_information['debug_mode']
 
 # oAuth2 information
@@ -41,12 +48,22 @@ auth_data = {
     'client_secret': clientSecret,
 }
 
+# Retrieve list of defined APIs
 apiList = []
 for current_api_info in api_information:
-    apiList.append(Api(ApiUrls(current_api_info["url"],
-                               f'{current_api_info["url"]}{current_api_info["test_endpoint"]}{current_api_info["test_value"]}'),
+    apiTestsList = []
+
+    # For each API, retrieve the list of test to execute
+    for current_api_test in current_api_info["tests"]:
+        apiTestsList.append(
+            ApiTests(current_api_test["test_name"], current_api_test["test_endpoint"], current_api_test["test_value"],
+                     current_api_test["field_to_check"], current_api_test["http_method"]))
+
+    # Put every API inside a LIST of API Objects
+    apiList.append(Api(current_api_info["url"],
                        current_api_info["x-api-key"], current_api_info["x-env"],
-                       current_api_info["test_value"], current_api_info["api_name"], current_api_info["field_to_check"]))
+                       current_api_info["api_name"], apiTestsList))
+
 
 # Is used to concert a string with a LocalDateTime to a python datetime format
 def convert_utc_string_date_to_datetime(date_to_convert):
@@ -66,32 +83,23 @@ def return_access_token(oAuth2Url, auth_data):
     return response.json()['access_token']
 
 
-# Will check that the response of the api for the entity recovery is coherent
-def api_response_test(api_call_response, field, api_name):
-    if api_call_response.status_code == 200 and (
-            field in api_call_response.json() or field in api_call_response.json()[0]):
-        print(f'{Fore.GREEN} {api_name} API Responded correctly {Style.RESET_ALL}')
-    else:
-        print(f'{Fore.RED} Error in  {api_name} API Call, please, check your deployment {Style.RESET_ALL}')
-
-
 # Will test the actuator/info endpoint of the API and will make a call to retrieve information
-def test_api_and_return_entity(url, api_key, x_env, api_name, token, version="v1"):
+def test_api_health_endpoint(url, api_key, x_env, api_name, token, version="v1"):
     # Available Shipment Legacy validation
     print(f'{Fore.BLUE}\n -------------------- Testing : {api_name} API -------------------- {Style.RESET_ALL}')
 
-    api_response = requests.get(f'{url.url}/actuator/info', headers={'Authorization': 'Bearer ' + token,
+    api_response = requests.get(f'{url}/actuator/info', headers={'Authorization': 'Bearer ' + token,
                                                                      'x-api-key': api_key,
                                                                      'x-env': x_env})
     if api_response.status_code != 200:
-        api_response = requests.get(f'{url.url}/api/{version}/actuator/info', headers={'Authorization': 'Bearer ' + token,
-                                                                         'x-api-key': api_key,
-                                                                         'x-env': x_env})
+        api_response = requests.get(f'{url}/api/{version}/actuator/info',
+                                    headers={'Authorization': 'Bearer ' + token,
+                                             'x-api-key': api_key,
+                                             'x-env': x_env})
     if debug_mode:
         print(api_response)
         print(api_response.json())
-        print(f'API URL : {url.url}')
-        print(f'Endpoint to test : {url.test_endpoint}')
+        print(f'API URL : {url}')
         print(f'Token : {token}')
         print(f'Auth Datas : {auth_data}')
 
@@ -100,24 +108,37 @@ def test_api_and_return_entity(url, api_key, x_env, api_name, token, version="v1
         version = api_response.json()['app']['version']
         build_date = datetime.strptime(convert_utc_string_date_to_datetime(api_response.json()['app']['build']['date']),
                                        '%Y-%m-%d %H:%M:%S')
-        print(f'{Fore.GREEN} Successfully retrieved {api_name} API information. {Style.RESET_ALL}')
+        print(f'{Fore.GREEN} Successfully retrieved {api_name} API information {Style.RESET_ALL}')
         if (currentTime - build_date).days >= 1:
             print(
-                f'{Fore.YELLOW} Warning : The version build date is not from today : {build_date} {Style.RESET_ALL}')
-
-        api_response = requests.get(f'{url.test_endpoint}/',
-                                    headers={'Authorization': 'Bearer ' + token,
-                                             'x-api-key': api_key,
-                                             'x-env': x_env})
-
-        if debug_mode:
-            print(json.dumps(api_response.json(), indent=2))
-
-        return api_response
+                f'{Fore.YELLOW} VERSION : {version}. \n Warning : The version build date is not from today : {build_date} {Style.RESET_ALL}')
+        else:
+            print(
+                f'{Fore.WHITE} VERSION : {version}. \n The version build date has been built recently : {build_date} {Style.RESET_ALL}')
 
     else:
         print(
             f'{Fore.RED} Error, cannot contact  {api_name} API - Please check your deployment. {Style.RESET_ALL}')
+
+
+# Test a real endpoint of the API and check if the given field is in the returned body
+def test_real_endpoint(current_api, test, access_token):
+    print(
+                f'{Fore.LIGHTGREEN_EX} ---------- Launching test : {test.name} ---------- {Style.RESET_ALL}')
+    api_response = requests.get(f'{current_api.url}/{test.test_endpoint}',
+                                headers={'Authorization': 'Bearer ' + access_token,
+                                         'x-api-key': current_api.key,
+                                         'x-env': current_api.env})
+    if debug_mode:
+        print(json.dumps(api_response.json(), indent=2))
+
+    if api_response.status_code == 200 and (
+            test.must_be_present_field in api_response.json() or test.must_be_present_field in api_response.json()[0]):
+        print(f'{Fore.GREEN} {current_api.name} - Test {test.name} executed successfully {Style.RESET_ALL}')
+    else:
+        print(f'{Fore.RED} Error in {current_api.name} - {test.name} API Call, please, check your deployment {Style.RESET_ALL}')
+
+    return api_response
 
 
 # Main function
@@ -125,9 +146,11 @@ def main():
     access_token = return_access_token(oAuth2Url, auth_data)
 
     for current_api in apiList:
-        response = test_api_and_return_entity(current_api.url, current_api.key, current_api.env, current_api.name,
-                                              access_token)
-        api_response_test(response, current_api.field_to_check, current_api.name)
+        test_api_health_endpoint(current_api.url, current_api.key, current_api.env, current_api.name,
+                                 access_token)
+
+        for test in current_api.tests:
+            test_real_endpoint(current_api, test, access_token)
 
 
 if __name__ == "__main__":
