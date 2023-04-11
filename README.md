17ec0a0c802b543b65124884a55abcb9622d2e3d
ANTHONY LEWANDOWSKI <lewandowski.anthony.pro@gmail.com>
2023-04-11 16:32:06 +0200

    feat(CaptainAPI) - Add multiple authentication and fulfill readme

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
 # CaptainApi
 
 ## What is Captain API ?
+
 Captain API is a small API tester which allows you to test if your API has been well deployed on you distant server.
 It supports oAuth2 authentication using Client ID/Secret.
 All the needed datas concerning your API must be filled inside the environment.json file
+You can test this project by launching the following API locally : https://github.com/LewandowskiAnthony/publicTestingAPI
+
 ## How to start ?
+
 ### Install python3
+
 First of all, the most important thing is to install you python environment on your engine.
-This project has been made using Python 3, so we recommend you to install the latest Python 3 version : 
+This project has been made using Python 3, so we recommend you to install the latest Python 3 version :
 https://www.python.org/downloads/
+
 ### Install needed package
+
 ```
 python3 -m pip install colorama requests datetime
 ```
+
 ### Fill the json environment file
+
 #### Fields
-The json environment file contain 3 different types of information : 
+
+The json environment file contain 3 different types of information :
 
 | Name             | Purpose                                                                                                                    | Example                                                                                                  |
 |------------------|----------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------------------------|
@@ -23,27 +33,75 @@ The json environment file contain 3 different types of information :
 | authentication   | All the informations you'll need to authenticate to your API using oAuth2 authentication                                   | the client_id                                                                                            |
 | apis             | This field in an array containing all your APIs informations. Captain API will check test every API mentioned in this part | url which is the URL of your API without the versioning                                                  |
 
-#### Example
+#### Authentication Definition example
+
+There's currently only two type of authentication supported by Captain API : oAuth2 with Client Grant Type and No auth. </br>
+No auth will be defined has shown below : 
+```
+  "authentication": {},
+```
+oAuth2 with Client grantType will be defined has shown below : 
+```
+  "authentication": {
+    "type" : "oAuth2",
+    "grantType": "Client",
+    "credential_name" : "My credential name to know who's the user",
+    "clientId": "XxXXxxxXX0000xxXXXXX00000000xxxxXXXxxxX00",
+    "client_secret": "XxXXxxxXX0000xxXXXXX00000000xxxxXXXxxxX00XXXXXXXXxxxxxXXxXXXXXXXX000",
+    "oAuth2Url": "https://my-auth-url.com/token",
+    "comment": "Here is a comment to make it more clear about where my token come from"
+  }
+```
+
+#### Api Definition example
+
 Here's an example to completion for an API test, admitting you want to test an api which follows this url : <br>
 http://my-super-api/api/v1/clients/1000
-Admitting this is the endpoint to test you'll have to fill the environment.json as : 
+Admitting this is the endpoint to test you'll have to fill the environment.json as :
+
 ```
 "apis": [
     {
       "url": "http:/my-super-api/",
-      "test_endpoint": "api/v1/clients/",
       "x-api-key": "YOUR_API_KEY",
-      "x-env": "YOU X-ENV",
-      "test_value": 1000,
-      "api_name": "My Super Api",
-      "field_to_check": "a field of your DTO to check it returns the right entity"
+      "x-env": "YOU X-ENV", 
+      "tests" : [
+        {
+          "test_name" : "This test does this action",
+          "test_endpoint": "api/v1/clients/",
+          "test_value" : 1,
+          "http_method" : "GET",
+          "field_to_check": "id"
+        }
+      ],
+      "api_name": "My Super Api"
     }
 ]
 ```
+
+| Attribute         | Definition                                                                                                | Facultative ? | Example               |
+|-------------------|-----------------------------------------------------------------------------------------------------------|---------------|-----------------------|
+| url               | Url of the API without endpoint. (Root of the /actuator/health endpoint)                                  | FALSE         | http:/my-super-api/   |
+| x-api-key         | Api Key of your API                                                                                       | FALSE         | xxxxx-xxxxxxxx-xxxxxx |
+| x-env             | API env to test                                                                                           | TRUE          | PREPROD               |
+| x-api-key         | Api Key of your API                                                                                       | FALSE         | xxxxx-xxxxxxxx-xxxxxx |
+| tests             | Array of tests to execute                                                                                 | TRUE          | {ARRAY OF OBJECTS}    |
+| skip_health_check | Skip the call to /actuator/health and /actuator/info, useful if you use an API that doesn't use standards | TRUE          | {ARRAY OF OBJECTS}    |
+| test_name         | Name of a test and what it does                                                                           | TRUE          | Retrieve a client     |
+| test_endpoint     | Endpoint to test (Starting after your API url)                                                            | TRUE          | /api/v1/client/       |
+| test_value        | Value to test (Like client ID)                                                                            | TRUE          | 1                     |
+| http_method       | Method of the test                                                                                        | TRUE          | GET                   |
+| field_to_check    | Fields in the return body to check if exists                                                              | TRUE          | id                    |      
+| api_name          | Name of the API to display                                                                                | FALSE         | My Awesome API        |           
+
 ### Start the project
-You can start the script using your command line : 
+
+You can start the script using your command line :
+
 ```
 python3 main.py environment.json
 ```
+
 ![img.png](img/img.png)
 
+Congratulation, you API is up !
--- main.py
+++ main.py
@@ -10,12 +10,20 @@ json_path = open(sys.argv[1])
 
 
 class Api:
-    def __init__(self, url, key, env, name, tests):
+    def __init__(self, url, key, env, name, tests, skip_health_check):
         self.url = url
         self.key = key
         self.env = env
         self.name = name
         self.tests = tests
+        self.skip_health_check = skip_health_check
+
+
+# Client credentials OAuth2 information
+class AuthenticationOAuth2ClientCredentials:
+    def __init__(self, url, auth_datas):
+        self.url = url
+        self.auth_data = auth_datas
 
 
 # Object containing API tests
@@ -39,19 +47,14 @@ api_information = data['apis']
 # Debug mode to activate logs
 debug_mode = running_information['debug_mode']
 
-# oAuth2 information
-clientId = auth_information['clientId']
-clientSecret = auth_information['client_secret']
-oAuth2Url = auth_information['oAuth2Url']
-auth_data = {
-    'client_id': clientId,
-    'client_secret': clientSecret,
-}
-
 # Retrieve list of defined APIs
 apiList = []
 for current_api_info in api_information:
     apiTestsList = []
+    if "skip_health_check" in current_api_info:
+        skip_health_check = current_api_info["skip_health_check"]
+    else:
+        skip_health_check = False;
 
     # For each API, retrieve the list of test to execute
     for current_api_test in current_api_info["tests"]:
@@ -62,7 +65,7 @@ for current_api_info in api_information:
     # Put every API inside a LIST of API Objects
     apiList.append(Api(current_api_info["url"],
                        current_api_info["x-api-key"], current_api_info["x-env"],
-                       current_api_info["api_name"], apiTestsList))
+                       current_api_info["api_name"], apiTestsList, skip_health_check))
 
 
 # Is used to concert a string with a LocalDateTime to a python datetime format
@@ -85,23 +88,23 @@ def return_access_token(oAuth2Url, auth_data):
 
 # Will test the actuator/info endpoint of the API and will make a call to retrieve information
 def test_api_health_endpoint(url, api_key, x_env, api_name, token, version="v1"):
-    # Available Shipment Legacy validation
+
     print(f'{Fore.BLUE}\n -------------------- Testing : {api_name} API -------------------- {Style.RESET_ALL}')
 
     api_response = requests.get(f'{url}/actuator/info', headers={'Authorization': 'Bearer ' + token,
-                                                                     'x-api-key': api_key,
-                                                                     'x-env': x_env})
+                                                                 'x-api-key': api_key,
+                                                                 'x-env': x_env})
     if api_response.status_code != 200:
         api_response = requests.get(f'{url}/api/{version}/actuator/info',
                                     headers={'Authorization': 'Bearer ' + token,
                                              'x-api-key': api_key,
                                              'x-env': x_env})
     if debug_mode:
-        print(api_response)
-        print(api_response.json())
+        if api_response:
+            print(api_response)
+            print(api_response.json())
         print(f'API URL : {url}')
         print(f'Token : {token}')
-        print(f'Auth Datas : {auth_data}')
 
     if api_response.status_code == 200:
         currentTime = datetime.now()
@@ -118,36 +121,61 @@ def test_api_health_endpoint(url, api_key, x_env, api_name, token, version="v1")
 
     else:
         print(
-            f'{Fore.RED} Error, cannot contact  {api_name} API - Please check your deployment. {Style.RESET_ALL}')
+            f'{Fore.RED} Error, cannot contact  {api_name} API health check - Please check your deployment of health '
+            f'endpoint. {Style.RESET_ALL}')
 
 
 # Test a real endpoint of the API and check if the given field is in the returned body
 def test_real_endpoint(current_api, test, access_token):
     print(
-                f'{Fore.LIGHTGREEN_EX} ---------- Launching test : {test.name} ---------- {Style.RESET_ALL}')
+        f'{Fore.LIGHTGREEN_EX} ---------- Launching test : {test.name} ---------- {Style.RESET_ALL}')
     api_response = requests.get(f'{current_api.url}/{test.test_endpoint}',
                                 headers={'Authorization': 'Bearer ' + access_token,
                                          'x-api-key': current_api.key,
                                          'x-env': current_api.env})
     if debug_mode:
-        print(json.dumps(api_response.json(), indent=2))
+        if api_response and api_response.json():
+            print(json.dumps(api_response.json(), indent=2))
 
     if api_response.status_code == 200 and (
             test.must_be_present_field in api_response.json() or test.must_be_present_field in api_response.json()[0]):
         print(f'{Fore.GREEN} {current_api.name} - Test {test.name} executed successfully {Style.RESET_ALL}')
     else:
-        print(f'{Fore.RED} Error in {current_api.name} - {test.name} API Call, please, check your deployment {Style.RESET_ALL}')
+        print(
+            f'{Fore.RED} Error in {current_api.name} - {test.name} API Call, please, check your deployment {Style.RESET_ALL}')
 
     return api_response
 
 
 # Main function
 def main():
-    access_token = return_access_token(oAuth2Url, auth_data)
+    access_token = '';
+    # Authentication information
+    if auth_information:
+        match auth_information['type']:
+            case 'oAuth2':
+                match auth_information['grantType']:
+                    case 'Client':
+                        auth_data = {
+                            'client_id': auth_information['clientId'],
+                            'client_secret': auth_information['client_secret']
+                        }
+                        authentication = AuthenticationOAuth2ClientCredentials(auth_information['oAuth2Url'], auth_data)
+                        access_token = return_access_token(authentication.url, authentication.auth_data)
+                    case 'Password':
+                        auth_data = {
+                            'username': auth_information['username'],
+                            'password': auth_information['password']
+                        }
+                    case _:
+                        print('No Grant Type')
+            case _:
+                print('No Authentication.')
 
     for current_api in apiList:
-        test_api_health_endpoint(current_api.url, current_api.key, current_api.env, current_api.name,
-                                 access_token)
+        if not current_api.skip_health_check:
+            test_api_health_endpoint(current_api.url, current_api.key, current_api.env, current_api.name,
+                                     access_token)
 
         for test in current_api.tests:
             test_real_endpoint(current_api, test, access_token)
